94925a4fafb9e0417eb1bb87fbf2c69b0d23f233
Kamil Ziętek <qamil1995@gmail.com>
2022-01-17 21:54:29 +0100

    Added very poor displayer of song

---
+++ SongChooser/SongView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SonglistGenerator;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SongChooser
+{
+    /// <summary>
+    /// Interaction logic for SongView.xaml
+    /// </summary>
+    public partial class SongView : Window
+    {
+        private readonly DataGrid dataGrid;
+        public Song SelectedSong => (this.dataGrid.SelectedItem as DisplaySong).song;
+
+        public SongView(DataGrid dataGrid)
+        {
+            this.dataGrid = dataGrid;
+
+            InitializeComponent();
+            this.text.Text = string.Join(Environment.NewLine, SelectedSong.Text);
+            this.chords.Text = string.Join(Environment.NewLine, SelectedSong.Chords);
+        }
+
+        private void LeftButtonClick(object sender, RoutedEventArgs e)
+        {
+            dataGrid.SelectedIndex--;
+        }
+
+        private void RightButtonClick(object sender, RoutedEventArgs e)
+        {
+            dataGrid.SelectedIndex++;
+        }
+    }
+}

4e84931a7c1aeb87144d4816fcc1710e3478aa14
Kamil Ziętek <qamil1995@gmail.com>
2022-01-20 19:50:29 +0100

    Moved converters to separate folder

--- SongChooser/Converters/ListOfStringToJoinedStringConverter.cs
+++ SongChooser/Converters/ListOfStringToJoinedStringConverter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Data;
 
-namespace SongChooser
+namespace SongChooser.Converters
 {
     public class ListOfStringToJoinedStringConverter : IValueConverter
     {
